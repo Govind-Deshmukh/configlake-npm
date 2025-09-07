90d31f67bf39b117ec69cfebca1973c9c454e76c
Govind Deshmukh <govind.ub47>
2025-09-07 18:43:05 +0530

    updated code

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,13 +2,13 @@ interface ConfigLakeResponse {
     configs: Record<string, string>;
     secrets: Record<string, string>;
 }
-export declare const getConfig: (baseUrl: string, token: string, projectId: number, environment: string) => Promise<Record<string, string>>;
-export declare const getSecrets: (baseUrl: string, token: string, projectId: number, environment: string) => Promise<Record<string, string>>;
-export declare const getAllDetails: (baseUrl: string, token: string, projectId: number, environment: string) => Promise<ConfigLakeResponse>;
+export declare function getConfig(baseUrl: string, token: string, projectId: number, environment: string): Promise<Record<string, string>>;
+export declare function getSecrets(baseUrl: string, token: string, projectId: number, environment: string): Promise<Record<string, string>>;
+export declare function getAllDetails(baseUrl: string, token: string, projectId: number, environment: string): Promise<ConfigLakeResponse>;
 declare const _default: {
-    getConfig: (baseUrl: string, token: string, projectId: number, environment: string) => Promise<Record<string, string>>;
-    getSecrets: (baseUrl: string, token: string, projectId: number, environment: string) => Promise<Record<string, string>>;
-    getAllDetails: (baseUrl: string, token: string, projectId: number, environment: string) => Promise<ConfigLakeResponse>;
+    getConfig: typeof getConfig;
+    getSecrets: typeof getSecrets;
+    getAllDetails: typeof getAllDetails;
 };
 export default _default;
 //# sourceMappingURL=index.d.ts.map
